2de2a515a3699813f88559114ace89b790133b15
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2023-05-14 12:04:48 +0200

    Initial commit

--- ad-test-computer-clean-up.cmd
+++ ad-test-computer-clean-up.cmd
@@ -7,20 +7,17 @@ echo -----------------------------
 
 echo AD Computer Create
 del /q "ad-computer-create\.terraform.lock.hcl"
-del /q "ad-computer-create\terraform.tfstate"
-del /q "ad-computer-create\terraform.tfstate.backup"
+del /q "ad-computer-create\terraform.tfstate.*"
 rmdir /s /q "ad-computer-create\.terraform"
 
 echo AD Computer List #1
 del /q "ad-computer-list-1\.terraform.lock.hcl"
-del /q "ad-computer-list-1\terraform.tfstate"
-del /q "ad-computer-list-1\terraform.tfstate.backup"
+del /q "ad-computer-list-1\terraform.tfstate.*"
 rmdir /s /q "ad-computer-list-1\.terraform"
 
 echo AD Computer List #2
 del /q "ad-computer-list-2\.terraform.lock.hcl"
-del /q "ad-computer-list-2\terraform.tfstate"
-del /q "ad-computer-list-2\terraform.tfstate.backup"
+del /q "ad-computer-list-2\terraform.tfstate.*"
 rmdir /s /q "ad-computer-list-2\.terraform"
 
 echo -----------------------------
